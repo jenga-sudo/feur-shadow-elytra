70694891c8ef32e584ec38295aa6c3fad89c3717
jenga-sudo <j53167364@gmail.com>
2025-10-03 19:59:27 +0900

    Update RegistryInit.java

--- src/main/java/com/example/feurshadowelytra/RegistryInit.java
+++ src/main/java/com/example/feurshadowelytra/RegistryInit.java
@@ -1,9 +1,17 @@
-
 package com.example.feurshadowelytra;
-import net.minecraft.world.item.*;
-import net.minecraftforge.registries.*;
+
+import net.minecraft.world.item.Item;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.minecraft.world.item.Items;
+
 public class RegistryInit {
-  public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, FeurShadowElytra.MODID);
-  public static final RegistryObject<Item> FEUR_WINGS_ELYTRA = ITEMS.register("feur_wings_elytra",
-    () -> new ShadowElytraItem(new Item.Properties().stacksTo(1).durability(Items.ELYTRA.getDefaultMaxDamage())));
+  public static final DeferredRegister<Item> ITEMS =
+      DeferredRegister.create(ForgeRegistries.ITEMS, FeurShadowElytra.MODID);
+
+  public static final RegistryObject<Item> FEUR_WINGS_ELYTRA = ITEMS.register(
+      "feur_wings_elytra",
+      () -> new ShadowElytraItem(new Item.Properties().stacksTo(1).durability(432))
+  );
 }
